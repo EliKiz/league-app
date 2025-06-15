a79e70ea1a79ff80f814eb9178a0a11e9110c5e6
Ilya Knyazev <knyazev_i@dogma.ru>
2025-06-15 18:50:36 +0600

    chore: added favicon

--- src/ui/Header/Header.tsx
+++ src/ui/Header/Header.tsx
@@ -1,9 +1,22 @@
-import { Typography } from 'antd';
+import { Typography } from "antd";
+import logo from "../../assets/sports_leagues_logo.svg";
+import "./Header.css";
 
 export const Header = () => (
-  <header style={{ padding: '24px 0', textAlign: 'center' }}>
-    <Typography.Title level={2} style={{ margin: 0 }}>
-      Sports Leagues
-    </Typography.Title>
+  <header style={{ padding: "24px 0", textAlign: "center" }}>
+    <div className="header__logo-row">
+      <Typography.Title
+        level={2}
+        style={{
+          margin: 0,
+          marginLeft: 16,
+          display: "inline-block",
+          verticalAlign: "middle",
+        }}
+      >
+        Sports Leagues
+      </Typography.Title>
+      <img src={logo} alt="Sports Leagues Logo" className="header__logo-img" />
+    </div>
   </header>
-); 
+);
